324f52c3bcda88a6adc8f7b386c61ce8b0e46c49
mayankesh239 <mayankesh.ss@gmail.com>
2021-12-23 12:28:53 +0530

    updated

--- js/index.js
+++ js/index.js
@@ -75,7 +75,7 @@ window.onload = function () {
     // INJECT CSS
     var css = document.createElement("style");
     css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff; font-size: 3vw}";
     document.body.appendChild(css);
 };
 
